a3a2ee29bdf727cc588ddc2f04601414b3043591
in2dblue <in2dblue.ocean@gmail.com>
2021-08-30 17:04:51 +0200

    Updated to annotate C or N

--- CWI Sequence Labeller/cefr_annotate.py
+++ CWI Sequence Labeller/cefr_annotate.py
@@ -8,21 +8,27 @@ def levels(word):
         df = all_levels.loc[all_levels['word'] == word]
         # level = df.iloc[0]['level']
         level = min(df['level'])
-        return level
+        if int(level) > 3:
+            return 'C'
+        else:
+            return 'N'
     except:
         try:
             df = all_levels.loc[all_levels['word'] == word]
             level = df.iloc[0]['level']
-            return level
+            if int(level) > 3:
+                return 'C'
+            else:
+                return 'N'
         except:
-            return 0
+            return 'N'
 
 
 all_levels = pd.read_table('cefr_levels.tsv', names=('word', 'level'))
 
-df = pd.read_csv("data/ONESTOP_TESTDATA.csv", usecols=["caption"])
+df = pd.read_csv("data/INS_OSE_TESTDATA.csv", usecols=["caption"])
 
-with open('data/ONESTOP_TESTDATA_annotated.tsv', 'wt') as out_file:
+with open('data/INS_OSE_TESTDATA_final.tsv', 'wt') as out_file:
     tsv_writer = csv.writer(out_file, delimiter='\t')
     for index, row in df.iterrows():
         sentence = row['caption'].split()
@@ -32,7 +38,7 @@ with open('data/ONESTOP_TESTDATA_annotated.tsv', 'wt') as out_file:
             if word != '.' and word[-1] == '.':
                 word = word[:-1]
                 tsv_writer.writerow([word, levels(word)])
-                tsv_writer.writerow(['.', '0'])
+                tsv_writer.writerow(['.', 'N'])
                 tsv_writer.writerow('')
             else:
                 tsv_writer.writerow([word, levels(word)])
