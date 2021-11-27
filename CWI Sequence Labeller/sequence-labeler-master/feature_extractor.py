b01dd06907514b550a3bfffffc9a55b729c1bc25
in2dblue <in2dblue.ocean@gmail.com>
2021-11-27 13:01:03 +0100

    Created training file with all features

--- CWI Sequence Labeller/sequence-labeler-master/feature_extractor.py
+++ CWI Sequence Labeller/sequence-labeler-master/feature_extractor.py
@@ -4,46 +4,6 @@ from nltk.corpus import wordnet
 from datamuse import datamuse
 
 
-# Convert tree bank tags to ones that are compatible w google
-def is_noun(tag):
-    return tag in ['NN', 'NNS', 'NNP', 'NNPS']
-
-
-def is_verb(tag):
-    return tag in ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
-
-
-def is_adverb(tag):
-    return tag in ['RB', 'RBR', 'RBS']
-
-
-def is_adjective(tag):
-    return tag in ['JJ', 'JJR', 'JJS']
-
-
-# def penn_to_wn(tag):
-#     if is_adjective(tag):
-#         return wn.ADJ
-#     elif is_noun(tag):
-#         return wn.NOUN
-#     elif is_adverb(tag):
-#         return wn.ADV
-#     elif is_verb(tag):
-#         return wn.VERB
-#     return None
-
-def penn_to_google(tag):
-    if is_adjective(tag):
-        return 'adj'
-    elif is_noun(tag):
-        return 'n'
-    elif is_adverb(tag):
-        return 'adv'
-    elif is_verb(tag):
-        return 'v'
-    return None
-
-
 class ExtractFeatures(object):
     def __init__(self, sentences=None):
         self.sentences = sentences
@@ -91,11 +51,15 @@ class ExtractFeatures(object):
     # @staticmethod
     def get_syllables(self, word):
         syllables = 0
-        word_results = self.api.words(sp=word, max=1, md='psf')
-        if len(word_results) > 0:
-            word = word_results[0]["word"]
-            syllables = int(word_results[0]["numSyllables"])
-        return syllables
+        try:
+            word_results = self.api.words(sp=word, max=1, md='psf')
+            if len(word_results) > 0:
+                word = word_results[0]["word"]
+                syllables = int(word_results[0]["numSyllables"])
+            return syllables
+        except:
+            print('Error to get syllables for: ' + word)
+            return syllables
 
     # BINARY FEATURES
     def ogdens_basic_english(self, word):
@@ -116,34 +80,3 @@ class ExtractFeatures(object):
             return value
         except:
             return 0.0
-
-    def get_google_freq(self, row):
-        nofreq = float(0.000000)
-        word = row["phrase"]
-        word = str(word)
-        tag = row["pos"]
-        tag = penn_to_google(tag)
-
-        try:
-            word_results = self.api.words(sp=word, max=1, md='pf')
-            tag_list = (word_results[0]['tags'][:-1])
-            frequency = word_results[0]['tags'][-1][2:]
-            frequency = float(frequency)
-            if tag in tag_list:
-                return frequency
-            else:
-                lemma = row['lemma']
-                try:
-                    word_results = self.api.words(sp=lemma, max=1, md='pf')
-                    tag_list = (word_results[0]['tags'][:-1])
-                    frequency = word_results[0]['tags'][-1][2:]
-                    frequency = float(frequency)
-                    if tag in tag_list:
-                        return frequency
-                    else:
-                        return nofreq
-                except:
-                    return nofreq
-        except:
-            return nofreq
-
